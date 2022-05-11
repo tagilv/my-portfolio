3643b7a44b7c63a6561c3689b4815d45530fa595
Viktor Tagil <viktor.tagil@gmail.com>
2022-05-11 10:41:00 +0200

    contact section added

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import "./contact.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faContactCard } from '@fortawesome/free-solid-svg-icons';
+import email from "../../img/email.svg"
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faContactCard } from '@fortawesome/free-solid-svg-icons';
+// <FontAwesomeIcon icon={faContactCard}></FontAwesomeIcon>
 
 function Contact() {
   return (
     <div className="contact">
       <div className="contact-bg"></div>
       <div className="contact-wrapper">
-          <div className="contact-left"></div>
+          <div className="contact-left">
             <h1 className="contact-title">Contact me here or send an email to viktor.tagil@gmail.com</h1>
-            <FontAwesomeIcon icon={faContactCard}></FontAwesomeIcon>
             <div className="contact-info">
                 <div className="contact-info-item">
                   <img
-                    src=""
-                    alt=""
+                    src={email}
+                    alt="viktor.tagil@gmail.com"
                     className="contact-icon"
                   />
                 </div>
             </div>
-          <div className="contact-right"></div>
+          </div>
+          <div className="contact-right">
+            <p className="contact-description">Message me below
+            </p>
+            <form>
+              <input type="text" placeholder="Name" name="user_name"></input>
+              <input type="text" placeholder="Subject" name="user_subject"></input>
+              <input type="text" placeholder="Email" name="user_email"></input>
+              <textarea rows="5" placeholder="Message" name="message"/>
+              <button>Submit</button>
+            </form>
+          </div>
       </div>
     </div>
   )
