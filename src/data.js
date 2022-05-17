8a91de07b5d501f5ca8d78bb9fc991be3119af2c
Viktor Tagil <viktor.tagil@gmail.com>
2022-05-17 14:02:23 +0200

    proper links added

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export const products = [
   {
     id: 1,
-    img: "https://europe1.discourse-cdn.com/figma/optimized/3X/2/b/2b6ac8518d2661c4571758428921dd7aa04f26c8_2_460x1000.jpeg",
+    img: "https://i.imgur.com/9BKne9o.png",
     link: "https://super-sponty.herokuapp.com/",
   },
   {
     id: 2,
-    img: "https://media-exp1.licdn.com/dms/image/C5603AQF6AIfzne6n7w/profile-displayphoto-shrink_400_400/0/1590674512986?e=1657756800&v=beta&t=GcOvdoE_4SGjDX3P8zul90z_ZCDhYREBCtxk93bq6e4",
-    link: "https://www.linkedin.com/in/viktortagil/",
+    img: "https://i.imgur.com/XJAvx03.jpg",
+    link: "https://ta-rental-service-rostysaurus.herokuapp.com/",
   },
   {
     id: 3,
